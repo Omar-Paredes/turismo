500b063bab1bbb9599956461d1c86e3be3c6e812
yacel100 <master.yac@gmail.com>
2023-04-04 13:19:50 +0000

    🐛 corrigiendo el problema del https

--- vr360/src/config.jsx
+++ vr360/src/config.jsx
@@ -1,8 +1,8 @@
 const config = {
   local: {
-    endpoint: "http://127.0.0.1:8000/api/",
-    urlPage: "http://127.0.0.1:5173/#/",
-    urlPrincipal: "http://127.0.0.1:4200/#/"
+    endpoint: "https://turismoapidev.cochabamba.bo/api/",
+    urlPage: "https://turismo360dev.cochabamba.bo/",
+    urlPrincipal: "https://turismodev.cochabamba.bo/"
   },
   production: {
     endpoint: "https://turismoapidev.cochabamba.bo/api/",
