0c88c94fea2c9c1ea275428abde22130cec314e5
Omar-Paredes <aomar.paredesc@gmail.com>
2023-04-05 18:24:11 -0400

    prueba de muestra de imagen

--- vr360/src/components/img360.jsx
+++ vr360/src/components/img360.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const Img360 = ({ data, inlineStyle, showDes }) => {
   return (
     <div style={inlineStyle} className="embed360 page" data-rotation="0 90 -10">
-      <a-scene className="RV">
-        <a-sky src={data.imagen}></a-sky>
-      </a-scene>
+      <Image
+          style={{width: '100%', height: '50%'}}
+          source={{uri:'https://engineering.fb.com/wp-content/uploads/2016/04/yearinreview.jpg'}}
+      />
 
       <div className={(showDes ? "aparecer " : "desaparecer ") + "content360"}>
         <h2 className="title">{data.nombre}</h2>
