179edf7f92ab348eb8d39f0472a45ff1001651dd
Omar-Paredes <aomar.paredesc@gmail.com>
2023-04-10 08:21:26 -0400

    arreglo de etiqueta Img a sky

--- vr360/src/components/img360.jsx
+++ vr360/src/components/img360.jsx
@@ -3,10 +3,9 @@ import React from "react";
 const Img360 = ({ data, inlineStyle, showDes }) => {
   return (
     <div style={inlineStyle} className="embed360 page" data-rotation="0 90 -10">
-      <Image
-          style={{width: '100%', height: '50%'}}
-          source={{uri:'https://engineering.fb.com/wp-content/uploads/2016/04/yearinreview.jpg'}}
-      />
+      <a-scene className="RV">
+        <a-sky src={data.imagen}></a-sky>
+      </a-scene>
 
       <div className={(showDes ? "aparecer " : "desaparecer ") + "content360"}>
         <h2 className="title">{data.nombre}</h2>
